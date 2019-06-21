54d94382556f88423ecfad11149eaae2eaca6fa8
lougaowei <lougaowei@chehejia.com>
2019-06-21 09:56:59 +0800

    update

--- app/src/main/java/com/lougw/ffmpeg/FFmpegPlayer.java
+++ app/src/main/java/com/lougw/ffmpeg/FFmpegPlayer.java
@@ -1,9 +1,18 @@
 package com.lougw.ffmpeg;
 
 public class FFmpegPlayer {
-
     static {
-
-
+        System.loadLibrary("Player");
+        System.loadLibrary("libavcodec");
+        System.loadLibrary("libavdevice");
+        System.loadLibrary("libavfilter");
+        System.loadLibrary("libavformat");
+        System.loadLibrary("libavutil");
+        System.loadLibrary("libpostproc");
+        System.loadLibrary("libswresample");
+        System.loadLibrary("libswscale");
     }
+
+    public native void playMyMedia(String url);
+
 }
